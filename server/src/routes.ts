eef0e212e455f34942182407eace1346292e1c3c
Lucas Moraes <lucasmoraes2@outlook.com.br>
2020-08-12 20:33:57 -0300

    First commit

---
+++ server/src/routes.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import knex from './database/connection';
+
+const routes = express.Router();
+
+// Listar usuários
+routes.get('/users', async (request, response) => {
+    const users = await knex('users').select('*');
+
+    return response.json(users);
+});
+
+// Listar um usuário
+routes.get('/users/:id', async (request, response) => {
+    const { id } = request.params;
+
+    const user = await knex('users').where('id', id).first();
+
+    return response.json(user);
+});
+
+// Inserir um usuário
+routes.post('/users', async (request, response) => {
+    const { name, email, phone } = request.body;
+
+    await knex('users').insert({
+        name,
+        email,
+        phone,
+    });
+
+    return response.json({ success: true });
+});
+
+// Atualizar um usuário
+routes.patch('/users/:id', async (request, response) => {
+    const { id } = request.params;
+    const { name, email, phone } = request.body;
+
+    await knex('users').where('id', id).update({
+        name,
+        email,
+        phone,
+    });
+
+    return response.json({ success: true });
+});
+
+// Deletar um usuário
+routes.delete('/users/:id', async (request, response) => {
+    const { id } = request.params;
+
+    await knex('users').delete(id);
+
+    return response.json({ success: true });
+});
+
+export default routes;
